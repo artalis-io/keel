3c5ebe7a35b3438f91adef20611c216b72e79f65
Mark Farkas <mark@artalis.io>
2026-05-23 09:19:10 +0200

    Bump version to 1.3.0

--- include/keel/keel.h
+++ include/keel/keel.h
@@ -41,11 +41,11 @@
 /** @brief Major version number. */
 #define KL_VERSION_MAJOR  1
 /** @brief Minor version number. */
-#define KL_VERSION_MINOR  2
+#define KL_VERSION_MINOR  3
 /** @brief Patch version number. */
 #define KL_VERSION_PATCH  0
 /** @brief Version string ("major.minor.patch"). */
-#define KL_VERSION_STRING "1.2.0"
+#define KL_VERSION_STRING "1.3.0"
 
 /** @} */
 
